1cdb8294d16ac9e34f5067023ffa0d7278a95b03
Hrithik <hritiks2012@gmail.com>
2022-09-27 08:38:21 +0530

    changes made

--- main.py
+++ main.py
@@ -24,23 +24,29 @@ health_data =health_data[health_length]
 data = pd.merge(pd.merge(energy_data , meat_data, on=['Code', 'Year']), health_data , on=['Code' , 'Year'])
 
 #for training purpose select USA and check correlation between GDP and different caategories
-data =data.loc[data['Code'].isin(['USA'])]
+data =data.loc[data['Code'].isin(["USA"])]             # add countries of your choice. 
 
-# replacing the NaN value of different attributes
-data =data.fillna({                 # Since there is no starting value for health expenditure, replace NaN value with mean
-    'health_expenditure':data['health_expenditure'].mean(),  
-    'Meat_quantity':data['Meat_quantity'].mean(),
-    'Energy_use ':data['Energy_use '].mean()
-})
-# automatic filling of na values with the middle or average values between two points for rest of the data
-data = data.interpolate()   
+# automatic filling of na values with ffill and bfill values between two points grouped by country for rest of the data
+# data = data.interpolate()   
+data["Meat_quantity"] = data.groupby('Entity')['Meat_quantity'].transform(lambda x: x.ffill().bfill())
+data["Energy_use "] = data.groupby('Entity')['Energy_use '].transform(lambda x: x.ffill().bfill())
+data["health_expenditure"] = data.groupby('Entity')['health_expenditure'].transform(lambda x: x.ffill().bfill())
+data["GDP "] = data.groupby('Entity')['GDP '].transform(lambda x: x.ffill().bfill())
+
+
+
+with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.precision', 6,
+                       ):
+                            print(data)
 
 #checking the null values
-print(data)    
+print(data.isnull().sum())
 
 x = data.loc[:,"Energy_use "]         #0.62 -> moderately correlated
 y = data.loc[:,"Meat_quantity"]         #0.275 -> Weakly  correlated
-z = data.loc[:,"health_expenditure"]    #0.512 ->strongly correlated
+z = data.loc[:,"health_expenditure"]    #0.97 ->strongly correlated
 g = data.loc[:,"GDP "]        
 print(np.corrcoef(z,g))
 
